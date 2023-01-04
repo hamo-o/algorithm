ae69d76d6559ccd3664c207cd3c0d3b94ec711df
hamo-o <yheel369@gmail.com>
2023-01-04 14:26:17 +0900

    boj 1931

--- greedy/1931.py
+++ greedy/1931.py
@@ -1,11 +1,21 @@
-#
-
-
-
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+times = []
 
+for i in range(n):
+    times.append(tuple(map(int, input().split())))
 
+times.sort(key=lambda x: (x[1], x[0]))
 
+end = times[0][1]
+cnt = 1
+times.pop(0)
+
+for time in times:
+    if end <= time[0]:
+        cnt += 1
+        end = time[1]
+
+print(cnt)
