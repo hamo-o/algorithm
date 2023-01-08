3ee39a5c915cbb0b5272c13b10b0d563fedc8c8c
hamo-o <yheel369@gmail.com>
2023-01-09 00:08:05 +0900

    boj 1449

--- greedy/1449.py
+++ greedy/1449.py
@@ -7,4 +7,14 @@ spot.sort()
 for i in range(n-1):
     gap.append(spot[i+1]-spot[i])
 
-print(gap)
+cnt = 0
+temp = 0
+for i in range(n-1):
+    temp += gap[i]
+    if temp + 1 > l:
+        cnt += 1
+        temp = 0
+
+cnt += 1
+
+print(cnt)
