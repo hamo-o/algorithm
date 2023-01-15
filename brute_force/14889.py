fc9069a85d10b93f4b36e29e5ab378718c7d73b4
hamo-o <yheel369@gmail.com>
2023-01-15 23:14:31 +0900

    boj 14889

--- brute_force/14889.py
+++ brute_force/14889.py
@@ -1,25 +1,58 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-
-li = []
+scores = {}
+people = []
+min_score = 99
 for i in range(n):
-    li.append(list(map(int, input().split())))
+    people.append(i+1)
+    temp = list(map(int, input().split()))
+    for j in range(n):
+        if i != j:
+            scores[(i+1, j+1)] = temp[j]
 
 
-def dfs(start_cnt, link_cnt):
-    if visited.count(1) == n//2:
-        print(start_cnt)
+def find_score(pick, visited, base, score):
+    if len(pick) == 2:
+        score.append(scores[tuple(pick)])
         return
 
-    for i in range(n):
-        for j in range(n):
-            if i != j and not visited[i] and not visited[j]:
-                visited[i] = visited[j] = 1
-                start_cnt += (li[i][j] + li[j][i])
-                dfs(start_cnt, 0)
-                visited[i] = 0
-                visited[j] = 0
-                start_cnt = 0
+    for i in base:
+        if not visited[i-1]:
+            pick.append(i)
+            visited[i-1] = 1
+            find_score(pick, visited, base, score)
+
+            pick.pop()
+            visited[i-1] = 0
 
 
-visited = [0]*n
-dfs(0, 0)
+def dfs(pick, visited):
+    if len(pick) == n//2:
+        start.append(pick)
+        start_score = []
+        link_score = []
+        find_score([], [0]*n, tuple(pick), start_score)
+        find_score([], [0] * n, tuple(set(people) - set(pick)), link_score)
+        new = sum(start_score) - sum(link_score)
+
+        global min_score
+        if abs(new) < min_score:
+            min_score = abs(new)
+        return
+
+    for i in range(1, n):
+        if not visited[i] and pick[-1] < i+1:
+            pick.append(i+1)
+            visited[i] = 1
+            dfs(pick, visited)
+
+            pick.pop()
+            visited[i] = 0
+
+
+start = []
+dfs([1], [0]*n)
+
+print(min_score)
