b95e92a0a2aa8e39d445677ea8251efaf2607332
hamo-o <yheel369@gmail.com>
2023-01-16 19:08:10 +0900

    boj 17829

--- divide_and_conquer/17829.py
+++ divide_and_conquer/17829.py
@@ -3,43 +3,66 @@ input = sys.stdin.readline
 
 n = int(input())
 
-li = []*n
+nums = []*n
 for i in range(n):
     line = list(map(int, input().split()))
-    li.append(line)
+    nums.append(line)
 
 
-def make_list(raw):
-    length = len(raw)
-    new = [[0 for _ in range(4)] for _ in range((length//2)**2)]
+def pooling(size, x, y):
+    mid = size // 2
 
-    for i in range(length):
-        if i % 2 == 0:
-            for j in range(length//2):
-                new[(length//4)*i+j][0] = raw[i][2*j]
-                new[(length//4)*i+j][1] = raw[i][2*j+1]
-        else:
-            for j in range(length//2):
-                new[(length//4)*(i-1)+j][2] = raw[i][2*j]
-                new[(length//4)*(i-1)+j][3] = raw[i][2*j+1]
+    if size == 2:
+        answer = [nums[x][y], nums[x+1][y], nums[x][y+1], nums[x+1][y+1]]
+        answer.sort()
+        return answer[-2]
 
-    return new
-
-
-def choose_second(ans, raw):
-    temp = []
-    for item in raw:
-        item.sort()
-        temp.append(item[2])
-        if len(temp) == 4:
-            ans.append(temp)
-            temp = []
-
-    if len(ans) == 1:
-        print(ans[0][2])
-        return
-
-    choose_second([], make_list(ans))
+    left_top = pooling(mid, x, y)
+    right_top = pooling(mid, x+mid, y)
+    left_bottom = pooling(mid, x, y+mid)
+    right_bottom = pooling(mid, x+mid, y+mid)
+    answer = [left_top, right_top, left_bottom, right_bottom]
+    answer.sort()
+    return answer[-2]
 
 
-choose_second([], make_list(li))
+print(pooling(n, 0, 0))
+
+
+
+
+
+# def make_list(raw):
+#     length = len(raw)
+#     new = [[0 for _ in range(4)] for _ in range((length//2)**2)]
+#
+#     for i in range(length):
+#         if i % 2 == 0:
+#             for j in range(length//2):
+#                 new[(length//4)*i+j][0] = raw[i][2*j]
+#                 new[(length//4)*i+j][1] = raw[i][2*j+1]
+#         else:
+#             for j in range(length//2):
+#                 new[(length//4)*(i-1)+j][2] = raw[i][2*j]
+#                 new[(length//4)*(i-1)+j][3] = raw[i][2*j+1]
+#
+#     return new
+#
+#
+# def choose_second(ans, raw):
+#     temp = []
+#     for item in raw:
+#         item.sort()
+#         temp.append(item[2])
+#         if len(temp) == 4:
+#             ans.append(temp)
+#             temp = []
+#
+#     if len(ans) == 1:
+#         print(ans[0][2])
+#         return
+#
+#     choose_second([], make_list(ans))
+#
+#
+# choose_second([], make_list(li))
