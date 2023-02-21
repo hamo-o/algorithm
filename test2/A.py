da750c09a7668388df80bdeabf92465aeb60ea7a
hamo-o <yheel369@gmail.com>
2023-02-21 16:40:16 +0900

    BOJ 1850

--- test2/A.py
+++ test2/A.py
@@ -1,10 +1,15 @@
 a, b = map(int, input().split())
 k = abs(a-b)
 
-if k == 0:
-    if a > b:
-        k = b
-    else:
-        k = a
+if a == 1 or b == 1:
+    print("1")
 
-print("1"*k)
+else:
+    if k == 0:
+        if a > b:
+            k = b
+        else:
+            k = a
+
+    if k < 10000001:
+        print("1"*k)
---
+++ test2/B.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = input().split()
+m = input().split()
+
+if m:
+    broken = list(map(int, input().split()))
+
