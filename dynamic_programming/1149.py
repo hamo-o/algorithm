2646156b590c9e1a53475c68385776deea111ce2
hamo-o <yheel369@gmail.com>
2023-03-01 19:23:37 +0900

    BOJ 1149

--- dynamic_programming/1149.py
+++ dynamic_programming/1149.py
@@ -1,16 +1,21 @@
 n = int(input())
 
+colors = []
+for i in range(n):
+    r, g, b = map(int, input().split())
+    colors.append([r, g, b])
+
 
 def memo_num():
-    memo = [0]*(n+1)
-    memo[1] = 1
+    memo = [[0, 0, 0] for _ in range(n)]
+    memo[0] = colors[0]
 
-    if n > 1:
-        memo[2] = 2
-        for i in range(3, n+1):
-            memo[i] = (memo[i-2] + memo[i-1]) % 15746
+    for i in range(1, n):
+        memo[i][0] = min(memo[i-1][1], memo[i-1][2]) + colors[i][0]
+        memo[i][1] = min(memo[i-1][0], memo[i-1][2]) + colors[i][1]
+        memo[i][2] = min(memo[i-1][0], memo[i - 1][1]) + colors[i][2]
 
-    return memo[n]
+    return min(memo[n-1])
 
 
 print(memo_num())
