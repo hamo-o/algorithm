0756d32df0407a9c6dde283daccfd663eb158cee
hamo-o <yheel369@gmail.com>
2023-04-11 17:03:29 +0900

    BOJ 27884

--- others/27884.py
+++ others/27884.py
@@ -1,46 +1,43 @@
-up_num = 5
-down_num = 11
+import sys
+from itertools import product
+input = sys.stdin.readline
 
+UP_STATIONS = 5
+DOWN_STATIONS = 11
+
+# n개의 역 m 길이의 롤러코스터 구간
 n, m = map(int, input().split())
-stations = []
 for _ in range(n):
-    stations.append(input())
+    input()
 
-# n개의 역 중에 m-1개의 변환이 있어야 한다
-# 변환 가능 수를 넘으면 그냥 그대로 출력
-# 이때, 롤러코스터 구간의 길이는 m이므로 이 구간은 묶어서 하나로 보고,
-# 롤러코스터 구간을 언제 시작하냐에 따라 경우의 수가 달라진다.
 
+# 0 1
+# 2개 중에서 3개를 중복을 허용해서 뽑기
+# 중복순열
+roads = list(product([0, 1], repeat=n))
+
+
+def check_len(nums):
+    cnt = 1
+    max_cnt = 1
+    prev = nums[0]
+    for num in nums:
+        if num != prev:
+            cnt += 1
+        else:
+            cnt = 1
+        max_cnt = max(max_cnt, cnt)
+        prev = num
+
+    return max_cnt
+
+
+result = 0
 if m == 1:
-    start = 1
+    result = UP_STATIONS**n + DOWN_STATIONS**n
 else:
-    start = n-m+1
-flags = [[] for _ in range(start)]
+    for road in roads:
+        if check_len(road) == m:
+            result += (UP_STATIONS**road.count(1) * DOWN_STATIONS**road.count(0))
 
-for s in range(start):
-    flag = False
-    cnt = m-1
-    for i in range(n):
-        if cnt and s <= i:
-            flags[s].append(flag)
-            flag = not flag
-            cnt -= 1
-        else:
-            flags[s].append(flag)
-
-answer_down = 0
-answer_up = 0
-for li in flags:
-    temp_down = 1
-    temp_up = 1
-    for f in li:
-        if f:
-            temp_down *= 11
-            temp_up *= 5
-        else:
-            temp_down *= 5
-            temp_up *= 11
-    answer_down += temp_down
-    answer_up += temp_up
-
-print((answer_down+answer_up) % (1000000000+7))
+print(result % (10**9+7))
